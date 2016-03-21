94d3444acb24eae6a1d42aca82546c7924036698
sobeycloud <sobeycloud@sobeyclouddeMacBook-Pro.local>
2016-03-21 20:19:41 +0800

    wrote a code file

---
+++ SCTableView/SCTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCTableView
+//
+//  Created by sobeycloud on 16/3/21.
+//  Copyright © 2016年 sobeycloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SCTableView/SCTableViewTests/SCTableViewTests.m
@@ -0,0 +1,39 @@
+//
+//  SCTableViewTests.m
+//  SCTableViewTests
+//
+//  Created by sobeycloud on 16/3/21.
+//  Copyright © 2016年 sobeycloud. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SCTableViewTests : XCTestCase
+
+@end
+
+@implementation SCTableViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
